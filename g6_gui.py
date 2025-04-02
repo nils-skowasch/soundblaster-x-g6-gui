41a73e629495918466c1580ed9971fff75a47286
Nils Skowasch <nils_s89@gmx.de>
2025-04-02 15:51:39 +0200

    - create a hello world window

--- g6_gui.py
+++ g6_gui.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import wx
 
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name}')
+
+    # Next, create an application object.
+    app = wx.App()
+
+    # Then a frame.
+    frm = wx.Frame(None, title="Hello World")
+
+    # Show it.
+    frm.Show()
+
+    # Start the event loop.
+    app.MainLoop()
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
